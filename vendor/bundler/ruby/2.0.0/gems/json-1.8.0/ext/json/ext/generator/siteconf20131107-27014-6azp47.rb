f5e9055c585c0879ca1b6e89f971f5d4c855f66a
rswisteria <wisteria.r.s@gmail.com>
2013-11-08 15:54:51 +0900

    fixed.

--- vendor/bundler/ruby/2.0.0/gems/json-1.8.0/ext/json/ext/generator/siteconf20131107-27014-6azp47.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131107-27014-11fgws"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
